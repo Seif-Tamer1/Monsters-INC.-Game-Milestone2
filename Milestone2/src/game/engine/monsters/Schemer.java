7cff15b6b705a7dafe4df0bdb47617bb4de4b605
mohabbasem <mohabbasem@localhost>
2026-04-12 20:35:06 +0200

    mm

--- Milestone2/src/game/engine/monsters/Schemer.java
+++ Milestone2/src/game/engine/monsters/Schemer.java
@@ -1,5 +1,6 @@
 package game.engine.monsters;
 
+import game.engine.Constants;
 import game.engine.Role;
 
 public class Schemer extends Monster {
@@ -10,5 +11,8 @@ public class Schemer extends Monster {
 
 	public void executePowerupEffect(Monster opponentMonster) {
 
+	}
+	public void setEnergy(int energy) {
+		super.setEnergy(energy+Constants.SCHEMER_STEAL);
 	}
 }
