a9d032778c4a7599bc2404f6874fd9f38e6b00bf
mohabbasem <mohabbasem@localhost>
2026-04-12 20:35:47 +0200

    mm

--- Milestone2/src/game/engine/monsters/Schemer.java
+++ Milestone2/src/game/engine/monsters/Schemer.java
@@ -1,7 +1,10 @@
 package game.engine.monsters;
 
+import java.util.ArrayList;
+
 import game.engine.Constants;
 import game.engine.Role;
+import game.engine.dataloader.DataLoader;
 
 public class Schemer extends Monster {
 
@@ -10,9 +13,22 @@ public class Schemer extends Monster {
 	}
 
 	public void executePowerupEffect(Monster opponentMonster) {
+		ArrayList<Monster> monsters = DataLoader.readMonsters();
+		
+
 
 	}
+
 	public void setEnergy(int energy) {
-		super.setEnergy(energy+Constants.SCHEMER_STEAL);
+		super.setEnergy(energy + Constants.SCHEMER_STEAL);
+	}
+
+	private int stealEnergyFrom(Monster target) {
+		if (target.getEnergy() < 10) {
+			return target.getEnergy();
+		} else {
+			return 10;
+		}
+
 	}
 }
